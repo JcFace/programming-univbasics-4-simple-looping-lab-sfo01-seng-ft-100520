23e019e33d6e771098d2de0e9924cc222b1b7c70
Jesse Vaughn <jcv221@gmail.com>
2020-09-11 02:50:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,13 +6,13 @@ def loop_message_five_times(string)
 end
 end
 
-#def loop_message_n_times(string, 4)
-  #counter = 0
-  #while string[counter] < 4 do
- # puts string
- # counter += 1
-#end
-#end 
+def loop_message_n_times(string, limit)
+  counter = 0
+  while counter < limit do
+  puts string
+  counter += 1
+end
+end 
 
 def output_array(array)
   counter = 0
@@ -24,7 +24,7 @@ end
 
 def return_string_array(array)
   counter = 0
-  while array[counter] do 
+  while counter < array.length do 
     return array.to_s
     counter += 1
   end
