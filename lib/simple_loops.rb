ecfacffe1796686ff18b1efd7dacf36f0521170e
Jesse Vaughn <jcv221@gmail.com>
2020-09-11 02:54:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,8 +24,9 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while counter < array.length do 
-    return array.to_s
+    new_array.push(array[counter].to_s)
     counter += 1
   end
 end 
